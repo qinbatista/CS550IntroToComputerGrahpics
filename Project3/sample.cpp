25c7f7bd439f9a1472939d077ebec521388be790
MacStudio <qin.batista@outlook.com>
2022-10-11 12:11:33 -0700

    update

--- Project3/sample.cpp
+++ Project3/sample.cpp
@@ -19,14 +19,12 @@
 #include "System/initialize.h"
 #include "System/controller.h"
 #include "System/menu.h"
+#include "System/display.h"
 #include "Objects/cessna.550"
 #include "Objects/plane.h"
 #include "Objects/bmptotexture.h"
 #include "Objects/Axes.h"
 #include "Objects/osusphere.h"
-
-unsigned char *TextureArray0, *TextureArray1; // global variables GLuint Tex0, Tex1;
-GLuint Tex0, Tex1;
 //	This is a sample OpenGL / GLUT program
 //
 //	The objective is to draw a 3d object and change the color of the axes
@@ -69,137 +67,68 @@ int main(int argc, char *argv[])
     // the following line is here to make the compiler happy:
     return 0;
 }
-void InitObjectsLists()
-{
-    glutSetWindow(MainWindow);
 
-
-    CreateAxis();
-    MyFirstObject();
-    // WireFrame();
-    // PolygonFrame();
-    // Propeller();
-    OsuSphere(1,1,1);
-
-
-
-    int width0, height0, width1, height1;
-    unsigned char *TextureArray0 = BmpToTexture("Texture/worldtex.bmp", &width0, &height0);
-    unsigned char *TextureArray1 = BmpToTexture("Texture/worldtex.bmp", &width1, &height1);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glGenTextures(1, &Tex0); // assign binding “handles”
-    glGenTextures(1, &Tex1);
-}
-// this is where one would put code that is to be called
-// everytime the glut main loop has nothing to do
-//
-// this is typically where animation parameters are set
-//
-// do not call Display( ) from here -- let glutPostRedisplay( ) do it
 void Animate()
 {
-    // put animation stuff in here -- change some global variables
-    // for Display( ) to find:
+
     int ms = glutGet(GLUT_ELAPSED_TIME); // milliseconds
     ms %= MS_IN_THE_ANIMATION_CYCLE;
     TimeCycle = (float)ms / (float)MS_IN_THE_ANIMATION_CYCLE; // [0., 1.]
     BladeAngle = TimeCycle * 360.;
-    // force a call to Display( ) next time it is convenient:
+
     glutSetWindow(MainWindow);
     glutPostRedisplay();
+}
+int Width, Height;
+unsigned char *Texture;
+GLuint Tex0, Tex1; // global variables
+void InitObjectsLists()
+{
+    glutSetWindow(MainWindow);
+
+    CreateAxis();
+    // MyFirstObject();
+    // WireFrame();
+    // PolygonFrame();
+    // Propeller();
+    Texture = BmpToTexture("Texture/worldtex.bmp", &Width, &Height);
+    int level = 0, ncomps = 3, border = 0;
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &Tex0);
+    glBindTexture(GL_TEXTURE_2D, Tex0);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, Width, Height, 0, GL_RGB, GL_UNSIGNED_BYTE, Texture);
+    OsuSphere(1,50,50);
 }
 
 // draw the complete scene:
 void Display()
 {
-    // set which window we want to do the graphics into:
-    glutSetWindow(MainWindow);
-    // erase the background:
-    glDrawBuffer(GL_BACK);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-#ifdef DEMO_DEPTH_BUFFER
-    if (DepthBufferOn == 0)
-        glDisable(GL_DEPTH_TEST);
-#endif
-    // specify shading to be flat:
-    glShadeModel(GL_FLAT);
-    // set the viewport to a square centered in the window:
-    GLsizei vx = glutGet(GLUT_WINDOW_WIDTH);
-    GLsizei vy = glutGet(GLUT_WINDOW_HEIGHT);
-    GLsizei v = vx < vy ? vx : vy; // minimum dimension
-    GLint xl = (vx - v) / 2;
-    GLint yb = (vy - v) / 2;
-    glViewport(xl, yb, v, v);
-    // set the viewing volume:
-    // remember that the Z clipping  values are actually
-    // given as DISTANCES IN FRONT OF THE EYE
-    // USE gluOrtho2D( ) IF YOU ARE DOING 2D !
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (WhichProjection == ORTHO)
-        glOrtho(-2.f, 2.f, -2.f, 2.f, 0.1f, 1000.f);
-    else
-        gluPerspective(70.f, 1.f, 0.1f, 1000.f);
-    // place the objects into the scene:
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    // set the eye position, look-at position, and up-vector:
-    // set the eye position, look-at position, and up-vector:
+    DisplaySetting();
 
-    // rotate the scene:
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, Tex0);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glCallList(osusphereList);
+    glDisable( GL_TEXTURE_2D );
 
-    glRotatef((GLfloat)Yrot, 0.f, 1.f, 0.f);
-    glRotatef((GLfloat)Xrot, 1.f, 0.f, 0.f);
+    // glEnable(GL_TEXTURE_2D);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
-    // uniformly scale the scene:
-
-    if (Scale < MINSCALE)
-        Scale = MINSCALE;
-    glScalef((GLfloat)Scale, (GLfloat)Scale, (GLfloat)Scale);
-
-    // set the fog parameters:
-
-    if (DepthCueOn != 0)
-    {
-        glFogi(GL_FOG_MODE, FOGMODE);
-        glFogfv(GL_FOG_COLOR, FOGCOLOR);
-        glFogf(GL_FOG_DENSITY, FOGDENSITY);
-        glFogf(GL_FOG_START, FOGSTART);
-        glFogf(GL_FOG_END, FOGEND);
-        glEnable(GL_FOG);
-    }
-    else
-    {
-        glDisable(GL_FOG);
-    }
-
-    // possibly draw the axes:
-
-    if (AxesOn != 0)
-    {
-        glColor3fv(&Colors[WhichColor][0]);
-        glCallList(AxesList);
-    }
-
-    // since we are using glScalef( ), be sure the normals get unitized:
-
-    glEnable(GL_NORMALIZE);
-
-
-    if (WhichViewMode == INSIDE)
-    {
-        gluLookAt(0, 1.2, 1, 0., 5, 10., 0., 1., 0.);
-    }
-    else
-    {
-        gluLookAt(16., 7., 15., 0., 0., 1.6, 0., 1., 0.);
-    }
-
+    // // glTexImage2D(GL_TEXTURE_2D, 0, 3, width0, height0, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureArray0);
+    // OsuSphere(1., 50, 50);
     // draw the box object by calling up its display list:
 
-    glCallList(BoxList);
-    glCallList(PlaneList);
+    // glCallList(BoxList);
+    // glCallList(PlaneList);
     // glCallList(PolygonList);
 
     // glPushMatrix();
@@ -232,55 +161,5 @@ void Display()
     // glScalef(1, 1, 1);
     // glCallList(BoxList);
     // glPopMatrix();
-
-
-
-#ifdef DEMO_Z_FIGHTING
-    if (DepthFightingOn != 0)
-    {
-        glPushMatrix();
-        glRotatef(90.f, 0.f, 1.f, 0.f);
-        glCallList(BoxList);
-        glPopMatrix();
-    }
-#endif
-
-    // draw some gratuitous text that just rotates on top of the scene:
-    // i commented out the actual text-drawing calls -- put them back in if you have a use for them
-    // a good use for thefirst one might be to have your name on the screen
-    // a good use for the second one might be to have vertex numbers on the screen alongside each vertex
-
-    glDisable(GL_DEPTH_TEST);
-    glColor3f(0.f, 1.f, 1.f);
-    // DoRasterString( 0.f, 1.f, 0.f, (char *)"Text That Moves" );
-
-    // draw some gratuitous text that is fixed on the screen:
-    //
-    // the projection matrix is reset to define a scene whose
-    // world coordinate system goes from 0-100 in each axis
-    //
-    // this is called "percent units", and is just a convenience
-    //
-    // the modelview matrix is reset to identity as we don't
-    // want to transform these coordinates
-
-    glDisable(GL_DEPTH_TEST);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.f, 100.f, 0.f, 100.f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glColor3f(1.f, 1.f, 1.f);
-    // DoRasterString( 5.f, 5.f, 0.f, (char *)"Text That Doesn't" );
-
-    // swap the double-buffered framebuffers:
-
-    glutSwapBuffers();
-
-    // be sure the graphics buffer has been sent:
-    // note: be sure to use glFlush( ) here, not glFinish( ) !
-
-    glFlush();
+    DisplayBuffer();
 }
-
-///////////////////////////////////////   HANDY UTILITIES:  //////////////////////////
